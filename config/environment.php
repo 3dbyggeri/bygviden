a0a01df431336d29fa4bc633334526781d796580
Thomas Watson Steen <w@tson.dk>
2009-03-31 17:52:37 +0200

    Added dynamic environment setting, defaulting to development (to set the current environment set the system environment variable BYGVIDEN_ENV)

--- config/environment.php
+++ config/environment.php
@@ -1,14 +1,21 @@
 <?php
 
+// set the current environment to development if the system environment variable BYGVIDEN_ENV is not set
+if($_ENV['BYGVIDEN_ENV'] == "")
+  $_ENV['BYGVIDEN_ENV'] = 'development';
+
 // load simple YAML parser
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../lib/spyc.php');
 
 // load the database.yml file
 $db = Spyc::YAMLLoad(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/../config/database.yml'));
 
+// scope the database settings to the correct environment
+$db = $db[$_ENV['BYGVIDEN_ENV']];
+
 // populate global database constants
-define('DATABASE_NAME', $db['development']['database']);
-define('DATABASE_HOST', $db['development']['host']);
-define('DATABASE_USER', $db['development']['username']);
-define('DATABASE_PASS', $db['development']['password']);
+define('DATABASE_NAME', $db['database']);
+define('DATABASE_HOST', $db['host']);
+define('DATABASE_USER', $db['username']);
+define('DATABASE_PASS', $db['password']);
 define('DATABASE_PREFIX', 'dev_');
